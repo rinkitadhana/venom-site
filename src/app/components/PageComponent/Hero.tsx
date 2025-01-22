7d481877b7bda2c4aeee0c9d6933293a665c6d4e
Rinkit Adhana <therinkit@gmail.com>
2025-01-22 21:41:18 +0530

    hero section added

---
+++ src/app/components/PageComponent/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section className=" relative h-lvh border border-black ">
+      hello
+      <button className="absolute bottom-32 right-32 text-xl font-medium px-3 py-2 rounded-sm border">
+        Discover Origins
+      </button>
+    </section>
+  )
+}
+
+export default Hero
